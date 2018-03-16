c5f1a0108126bb397c2b86e13b33ebd619e4eacf
Hephixor <wsergeant.t@gmail.com>
2018-03-16 18:50:49 +0100

    update readme test

---
+++ oak_useless_chaining/target/debug/deps/check_useless_chaining-6d80cd660ce5e46f.d
@@ -0,0 +1,5 @@
+/home/skylab/UPMC/M1S2/PSTL/oak_useless_chaining/target/debug/deps/check_useless_chaining-6d80cd660ce5e46f: src/main.rs
+
+/home/skylab/UPMC/M1S2/PSTL/oak_useless_chaining/target/debug/deps/check_useless_chaining-6d80cd660ce5e46f.d: src/main.rs
+
+src/main.rs:
